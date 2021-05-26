7d09ea797719bf54227ac796303618f547cfcd66
GingerGuy93 <alwayssunny93@gmail.com>
2021-05-26 15:31:03 -0400

    Created sweepstakes function that puts contestants in a nested dictionary by their registration numbers then pick a randomly selected registration number to get a winner.

--- contestant.py
+++ contestant.py
@@ -4,3 +4,6 @@ class Contestant:
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
+
+    def notify_winner(self, is_winner):
+        pass
--- sweepstake.py
+++ sweepstake.py
@@ -1,1 +1,21 @@
+from user_interface import UserInterface
+import random
+
+
 class Sweepstake:
+    def __init__(self, name):
+        self.contest_name = name
+        self.contestants = {}
+        self.user_interface = UserInterface()
+
+    def register_contestant(self, contestant):
+        self.contestants.update({f'{contestant.registration_num}': contestant})
+
+    def pick_winner(self, contestant):
+        selected_winner = random.choice(list(contestant.registration_num))
+        return selected_winner
+
+    def print_contestant_info(self, contestant):
+        pass
+
+
